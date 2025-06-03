1a107ca16436df88d87bd67438c51d53858cb605
mgapp <mgapp@MIKEGYO>
2025-06-03 18:15:49 -0400

    Add project files.

---
+++ ListHW/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ListHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> students = new List<string>();
+            string answer = "";
+            do
+            {
+                Console.WriteLine("Please enter name of student");
+                answer = Console.ReadLine();
+                students.Add(answer);
+                Console.WriteLine("Is that all students? Answer 'yes' if so, 'no' if not.");
+                answer = Console.ReadLine();
+            }
+            while (answer != "yes");
+            Console.WriteLine(students.Count);
+        }
+    }
+}
